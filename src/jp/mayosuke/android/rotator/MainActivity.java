51dc6f9bb3c750d548b7d12eeb3313ef4edb72b9
mayosuke <mayo.suke@gmail.com>
2013-01-31 00:02:25 +0900

    onCreateで自動回転設定がONだったらトグルボタンをONに、OFFだったらOFFにするようにした。

--- src/jp/mayosuke/android/rotator/MainActivity.java
+++ src/jp/mayosuke/android/rotator/MainActivity.java
@@ -1,12 +1,13 @@
 package jp.mayosuke.android.rotator;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
-import android.app.Activity;
-import android.content.res.Configuration;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
@@ -18,9 +19,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ToggleButton toggleButton = (ToggleButton)findViewById(R.id.toggleButton1);
+
         try {
             final int accelerometerRotation = android.provider.Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION);
             Log.i(TAG, "accelerometerRotation=" + accelerometerRotation);
+            final boolean isAutoRotationEnabled = (accelerometerRotation == 1 ? true : false);
+            toggleButton.setChecked(isAutoRotationEnabled);
+
             final int userRotation = android.provider.Settings.System.getInt(getContentResolver(), Settings.System.USER_ROTATION);
             Log.i(TAG, "userRotation=" + userRotation);
         } catch (SettingNotFoundException e) {
